a830437419d1ad64ab6634e0cc5de69d6e22a606
Ida Delphine <idadelm@gmail.com>
2024-05-22 21:38:33 +0100

    add ability to change brush color

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.drawingapp
 
 import android.app.Dialog
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
 import android.widget.SeekBar
 import android.widget.TextView
@@ -10,19 +11,29 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var drawingView: DrawingView
     private lateinit var brushButton: ImageButton
+    private lateinit var purpleButton: ImageButton
+    private lateinit var redButton: ImageButton
+    private lateinit var greenButton: ImageButton
+    private lateinit var orangeButton: ImageButton
+    private lateinit var blueButton: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
         brushButton = findViewById(R.id.brush_button)
+        purpleButton = findViewById(R.id.purple_button)
+        greenButton = findViewById(R.id.green_button)
+        redButton = findViewById(R.id.red_button)
+        orangeButton = findViewById(R.id.orange_button)
+        blueButton = findViewById(R.id.blue_button)
 
         drawingView = findViewById(R.id.drawing_view)
         drawingView.changeBrushSize((23.toFloat()))
@@ -30,6 +41,12 @@ class MainActivity : AppCompatActivity() {
         brushButton.setOnClickListener {
             showBrushChooserDialog()
         }
+
+        purpleButton.setOnClickListener(this)
+        greenButton.setOnClickListener(this)
+        redButton.setOnClickListener(this)
+        orangeButton.setOnClickListener(this)
+        blueButton.setOnClickListener(this)
 
     }
 
@@ -55,5 +72,25 @@ class MainActivity : AppCompatActivity() {
             }
         })
         brushDialog.show()
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.purple_button -> {
+                drawingView.setColor("#D14EF6")
+            }
+            R.id.red_button -> {
+                drawingView.setColor("#F37684")
+            }
+            R.id.orange_button -> {
+                drawingView.setColor("#EFB041")
+            }
+            R.id.green_button -> {
+                drawingView.setColor("#2DC40B")
+            }
+            R.id.blue_button -> {
+                drawingView.setColor("#2F6FF1")
+            }
+        }
     }
 }
