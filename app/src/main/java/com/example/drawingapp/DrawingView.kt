dfb01acdc6ee40a21757580c5496cda8f1567477
Ida Delphine <idadelm@gmail.com>
2024-05-22 18:50:29 +0100

    Make drawing persist on screen

--- app/src/main/java/com/example/drawingapp/DrawingView.kt
+++ app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -7,6 +7,7 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
@@ -22,9 +23,66 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
     private lateinit var canvas: Canvas
     private lateinit var canvasBitmap: Bitmap
     private var brushSize: Float = 0.toFloat()
+    private  val paths = mutableListOf<FingerPath>()
 
     init {
         setUpDrawing()
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(canvasBitmap)
+    }
+
+    // this function will be called by the system when the user is going to touch the screen
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        when (event?.action) {
+            // this event will be fired when the user put their finger on the screen
+            MotionEvent.ACTION_DOWN -> {
+                drawPath.color = color
+                drawPath.brushThickness = brushSize.toFloat()
+
+                drawPath.reset() // reseting path before setting initial point
+                drawPath.moveTo(touchX!!, touchY!!)
+            }
+
+            // this event will be fired when the user starts to move their finger; this will be
+            // fired continuously until the user picks up the finger
+            MotionEvent.ACTION_MOVE -> {
+                drawPath.lineTo(touchX!!, touchY!!)
+            }
+
+            // this event will be fired when the user will pick up the finger from their screen
+            MotionEvent.ACTION_UP -> {
+                drawPath = FingerPath(color, brushSize)
+                paths.add(drawPath)
+            }
+            else -> return false
+        }
+        invalidate() // refresh layout to reflect the drawing
+        return true
+
+        return super.onTouchEvent(event)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas?.drawBitmap(canvasBitmap, 0f, 0f, drawPaint)
+
+        for (path in paths) {
+            drawPaint.strokeWidth = path.brushThickness
+            drawPaint.color = path.color
+            canvas.drawPath(path, drawPaint)
+        }
+        if (!drawPath.isEmpty) {
+            drawPaint.strokeWidth = drawPath.brushThickness
+            drawPaint.color = drawPath.color
+            canvas.drawPath(drawPath, drawPaint)
+        }
     }
 
     private fun setUpDrawing() {
@@ -34,8 +92,10 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         drawPaint.style = Paint.Style.STROKE
         drawPaint.strokeJoin = Paint.Join.ROUND
         drawPaint.strokeCap = Paint.Cap.ROUND
+
+        canvasPaint = Paint(Paint.DITHER_FLAG)
         brushSize = 20.toFloat()
     }
 
-    internal inner class FingerPath(val color: Int, val brushThrickness: Float) : Path()
+    internal inner class FingerPath(var color: Int, var brushThickness: Float) : Path()
 }
